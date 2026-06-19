931e4e0f377581aaed1d2993ec035247c9905c3f
Aditi Nim <nimaditi7@gmail.com>
2026-06-19 15:56:50 +0530

    callbacks

--- day1/src/main.js
+++ day1/src/main.js
@@ -1,52 +1,63 @@
 import './style.css'
 import gsap from 'gsap'
 
-// gsap.to('.box', {
-//   x: 800,
-//   duration: 1.5,
-//   delay: 0.6,
-//   ease: "power2"
-// })
-
-
-// gsap.to('.box', {
-//   x: 800,
-//   duration: 1.5,
-//   delay: 0.6,
-//   ease: "elastic"
-// })
-
-
-// gsap.to('.box', {
-//   x: 800,
-//   duration: 1.5,
-//   delay: 0.6,
-//   ease: "bounce.inOut"
-// })
-
-
-// gsap.to('.box', {
-//   x: 800,
-//   duration: 1.5,
-//   delay: 0.6,
-//   ease: "steps(12)",
-//   repeat: -1,
-// })
-
-
 
 gsap.set(".box", {
   x: -300,
 })
 
-gsap.to('.box', {
-  x: 1500,
-  duration: 1,
-  delay: 0.6,
-  ease: "power2.inOut",
-  repeat: -1,
-  // yoyo: true
-})
 
+// gsap.to('.box', {
+//   x: 500,
+//   delay: 0.6,
+//   duration: 1.4,
+//   ease: "power2.inOut",
+
+
+//   onStart: ()=>{
+//     console.log("Animation started");
+//   },
+
+//   onComplete: ()=>{
+//     console.log("Animation completed adi!");
+    
+//   }
+// })
+
+// gsap.to('.box', {
+//   x: 500,
+//   delay: 0.6,
+//   duration: 1.4,
+//   ease: "power2.inOut",
+
+
+//   onStart: ()=>{
+//     console.log("Animation started");
+//   },
+
+//   onComplete: ()=>{
+//     gsap.to(".box", {
+//       y:300
+//     })
+    
+//   }
+// })
+
+
+gsap.to('.box', {
+  x: 500,
+  delay: 0.6,
+  duration: 1.4,
+  ease: "power2.inOut",
+
+
+  onStart: ()=>{
+    console.log("Animation started");
+  },
+
+  onUpdate: ()=>{
+    console.log("frames");
+  }
+})
 
 
