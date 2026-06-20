70969cdd9a2ed7788a31f1424ef41864359df4a4
Aditi Nim <nimaditi7@gmail.com>
2026-06-20 19:55:54 +0530

    multiple elements selection using one ref

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -3,54 +3,28 @@ import React , {useRef} from 'react'
 import gsap from 'gsap'
 import AnimateOnX from './components/AnimateOnX';
 
-// gsap.registerPlugin(useGSAP);
 
 const App = () => {
 
-  const boxRef= useRef(null);
+  const boxRef= useRef([]);
   const containerRef= useRef(null);
 
-  // const {contextSafe}= useGSAP(()=>{
-  //   gsap.to(boxRef.current, {
-  //     x: 700,
-  //     duration: 0.8,
-  //     delay: 0.6
-  //   })
-  // }, {scope: containerRef.current, dependencies: [], revertOnUpdate: true})
-  
+
   useGSAP(()=>{
     gsap.to(boxRef.current, {
       x: 700,
       duration: 1,
-      delay: 0.5
+      delay: 0.5,
+      stagger: 0.2
     })
   }, {scope: containerRef.current, dependencies: [], revertOnUpdate: true})
-  
-
-  // return (
-  //   <div ref={containerRef }>
-  //     <div ref={boxRef} className="box"></div>
-  //     <button onClick={(contextSafe(()=>{
-  //       // gsap.to()
-  //     }))}></button>
-  //   </div>
-  // )
 
   return (
     <div ref={containerRef }>
-      <div ref={boxRef} className="box"></div>
+      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
+      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
+      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
 
-      <AnimateOnX>
-        <div className="box"></div>
-      </AnimateOnX>
-
-      <AnimateOnX>
-        <div className="box1"></div>
-      </AnimateOnX>
-
-      <AnimateOnX>
-        <div className="box2"></div>
-      </AnimateOnX>
     </div>
   )
 }
