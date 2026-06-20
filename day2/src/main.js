5862fcaad0ad9b3ceee5d47c6a4682094fedf08e
Aditi Nim <nimaditi7@gmail.com>
2026-06-20 15:36:35 +0530

    splittext pluggin

--- day2/src/main.js
+++ day2/src/main.js
@@ -1,43 +1,23 @@
 import './style.css'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText, ScrollTrigger);
 
-gsap.set(".imageDiv", {
-  scale: 0.3
-})
-
-
-gsap.set(".content", {
-  gap: "50rem",
-})
-
-
-const tl= gsap.timeline({
-  scrollTrigger: {
-    trigger: ".page2", 
-    start: "top top",
-    end: "top -40%",
-    scrub: true , 
-    pin: true,  //jab tk animation poora nah ho, page scroll nah ho
-  }
+const split= new SplitText(".title h1", {
+  type: "chars, words, lines",
+  wordsClass: 'titleWord',
+  charsClass: "titleChars"
 });
 
-
-tl.to(".imageDiv", {
-  scale: 1,
-  // duration: 1.3,
-  ease: "power4.out", 
-  
-}).to('.content', {
-  gap: "2rem"
-}, "<")
-
-// scroll trigger- > start , triggering element, end 
-/**
- * pattern to write start and end
- * "___ ___"
- * first-> top , bottom, center (triggering element)
- * second-> top, bottom, center, 0%-100% (viewport)
- */
+gsap.from(split.chars, {
+  yPercent: -50,
+  opacity:0,
+  duration: 1,
+  ease: "expo.out",
+  stagger: {
+    each: 0.2,
+    from: "start"
+  }
+})
