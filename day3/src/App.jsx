823a4295c068f6149ccb40131e59674a64fe5f6b
Aditi Nim <nimaditi7@gmail.com>
2026-06-21 00:35:32 +0530

    framer motion

--- day3/src/App.jsx
+++ day3/src/App.jsx
@@ -1,32 +1,76 @@
 import { useGSAP } from '@gsap/react';
-import React , {useRef} from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap'
-import AnimateOnX from './components/AnimateOnX';
+import { motion, scale } from 'motion/react';
+
+
 
 
 const App = () => {
 
-  const boxRef= useRef([]);
-  const containerRef= useRef(null);
+  const containerVariant= {
+    hidden: {},
+    visible: {}
+  }
 
+  const boxVariant = {
+    hidden: {
+      opacity: 0,
+    },
 
-  useGSAP(()=>{
-    gsap.to(boxRef.current, {
-      x: 700,
-      duration: 1,
-      delay: 0.5,
-      stagger: 0.2
-    })
-  }, {scope: containerRef.current, dependencies: [], revertOnUpdate: true})
+    visible: {
+      opacity: 1
+    }
+  }
 
   return (
-    <div ref={containerRef }>
-      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
-      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
-      <div ref={(el)=> boxRef.current.push(el)} className="box"></div>
+    <div>
+      <motion.div variants={containerVariant} initial="hidden" animate="visible" className="containerDiv">
+        {/* <div className="page"></div> */}
+        <motion.div
+          variants={boxVariant}
+          
+          transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
+          
+          // whileHover={{scale:1.5}}
+          // whileTap={{scale: 0.8}}
+          // whileInView={{opacity:1, scale:0.5}}
+          // viewport={{once: false, amount:1}}
+          className="box">
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 0, opacity: 0, scale: 0.7 }}
+          transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
+          animate={{ x: 500, opacity: 1 }}
+          // whileHover={{scale:1.5}}
+          // whileTap={{scale: 0.8}}
+          // whileInView={{opacity:1, scale:0.5}}
+          // viewport={{once: false, amount:1}}
+          className="box">
+        </motion.div>
+
+        <motion.div
+          initial={{ x: 0, opacity: 0, scale: 0.7 }}
+          transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
+          animate={{ x: 500, opacity: 1 }}
+          // whileHover={{scale:1.5}}
+          // whileTap={{scale: 0.8}}
+          // whileInView={{opacity:1, scale:0.5}}
+          // viewport={{once: false, amount:1}}
+          className="box">
+        </motion.div>
+        {/* <div className="page"></div> */}
+      </motion.div>
+
+
 
     </div>
   )
 }
 
 export default App
+
+
+
+//animate me daalnege final property
