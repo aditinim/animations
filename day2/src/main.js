e687f619ccc9eae30b848a243a1d2ef070886967
Aditi Nim <nimaditi7@gmail.com>
2026-06-20 16:28:05 +0530

    car show

--- day2/src/main.js
+++ day2/src/main.js
@@ -13,14 +13,30 @@ gsap.registerPlugin(SplitText, ScrollTrigger, Draggable,InertiaPlugin, Flip);
 
 
 const img= document.querySelector(".specialImage");
+const img2= document.querySelector(".specialImage2");
+
 
 
 img.addEventListener('click', ()=>{
   const state= Flip.getState(img);
+  const state2= Flip.getState(img2);
+
+  document.querySelector(".imageGallery").appendChild(img2);
   document.querySelector(".imageShow").appendChild(img);
 
   Flip.from(state , {
     duration: 0.8,
-    ease: "power3.inOut"
+    ease: "power3.inOut",
+    absolute: true,
+    scale: true 
+  });
+
+  Flip.from(state2 , {
+    duration: 0.8,
+    ease: "power3.inOut",
+    absolute: true,
+    scale: true 
   })
+
+
 })
